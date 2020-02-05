8773486b5b8af9aa417d12b1f6924d1c2e651acb
DDouble-H <60542966+DDouble-H@users.noreply.github.com>
2020-02-05 23:51:40 +0900

    Create quick.py

---
+++ Algorithms/Sorting/quick.py
@@ -0,0 +1,27 @@
+def quick(x):
+    
+    if len(x) <= 1:
+        return x
+    
+    pivot = x[len(x)//2]
+    s_arr, m_arr, b_arr = [], [], []
+    
+    for i in x:
+        if i < pivot:
+            s_arr.append(i)
+        elif i > pivot:
+            b_arr.append(i)
+        else:
+            m_arr.append(i)
+            
+    x = quick(s_arr) + m_arr + quick(b_arr)
+    
+    return x
+
+import random
+
+before = random.sample(range(1, 100), 10)
+print('list', before)
+
+after = quick(before)
+print('sorted', after)
