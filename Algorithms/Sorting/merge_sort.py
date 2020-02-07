8b0fa1f9ef1fc4c14df2062af4b79402efd08698
DDouble-H <60542966+DDouble-H@users.noreply.github.com>
2020-02-08 02:19:40 +0900

    Update merge_sort.py

--- Algorithms/Sorting/merge_sort.py
+++ Algorithms/Sorting/merge_sort.py
@@ -3,27 +3,41 @@ def devide(x):
     if len(x) <= 1:
         return x
     
-    sorted_list = []
-    
-    mid_v = (len(x)//2) #중간
-    
-    left_v = x[:mid_v]#왼쪽
-    right_v = x[mid_v:] #오른쪽
-    
+    mid_v = (len(x)//2)
+    left_v = x[:mid_v]
+    right_v = x[mid_v:]
     left = devide(left_v)
     right = devide(right_v)
     
-    return devide(left), devide(right)
+    return merge(left, right)
 
-def merge():
-    pass
+def merge(left, right):
+    sorted_list = []
+    i = 0
+    j = 0
+    
+    while (i < len(left)) and (j < len(right)):
+        if left[i] < right[j]:
+            sorted_list.append(left[i])
+            i += 1
+        else:
+            sorted_list.append(right[j])
+            j += 1
+    while (i < len(left)):
+            sorted_list.append(left[i])
+            i += 1
+    while (j < len(right)):
+            sorted_list.append(right[j])
+            j += 1
+    
+    return sorted_list
 
 if __name__ == '__main__':
     
     import random
     
     before = random.sample(range(1, 100), 10)
-#   print('list', before)
+    print('list', before)
     
-    after = devide(before)
-#   print('list', after)
+    before = devide(before)
+    print('sorted_list', before)
