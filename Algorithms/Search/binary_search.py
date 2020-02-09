b24d37408adac9d0f726595b364df0cf1b562333
DDouble-H <60542966+DDouble-H@users.noreply.github.com>
2020-02-10 00:19:08 +0900

    Update binary_search.py

--- Algorithms/Search/binary_search.py
+++ Algorithms/Search/binary_search.py
@@ -1,17 +1,17 @@
 def binary_search(data, target):
-    
+
     start = 0
     end = len(data)-1
-    mid = len(data)//2
-    
-    if data[mid] > target:
-        # mid : 최댓값
-        end = mid-1
-    elif data[mid] < target:
-        # mid : 최솟값
-        start = mid+1
-    else:
-        return mid
+    mid = len(data)//2 
+
+    while data[mid] != target:
+        mid = (start+end)//2
+        if data[mid] > target:
+            end = mid-1
+        elif data[mid] < target:
+            start = mid+1
+        else:
+            return mid
 
 
 if __name__ == '__main__':
@@ -22,3 +22,4 @@ if __name__ == '__main__':
     target = data[2]
     
     after = binary_search(data, target)
+    print(after)
